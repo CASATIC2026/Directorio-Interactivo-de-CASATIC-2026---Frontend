f89850698ec5802a367947acf63ff3effd0bf8d0
AlanPineda2 <132850598+AlanPineda2@users.noreply.github.com>
2026-03-13 13:57:48 -0600

    he subido cambios en la edicion de frontend

--- src/pages/public/PresentacionPage.jsx
+++ src/pages/public/PresentacionPage.jsx
@@ -8,6 +8,28 @@ import slide3 from '../public/img/witsa2.png';
 import slide4 from '../public/img/ales2.png';
 import slide5 from '../public/img/aleti logo.png';
 
+import imagen1 from './img/socios e inversionistas/usam universidad alberto masferrer.png';
+import imagen2 from './img/socios e inversionistas/unplug studio.png';
+import imagen3 from './img/socios e inversionistas/univo.png';
+import imagen4 from './img/socios e inversionistas/universidad pedagogica.png';
+import imagen5 from './img/socios e inversionistas/universidad matias elgado.png';
+import imagen6 from './img/socios e inversionistas/universidad gerardo barrios.png';
+import imagen7 from './img/socios e inversionistas/universidad francisco gavidia.png';
+import imagen8 from './img/socios e inversionistas/universidad evangelica de el salvador.png';
+import imagen9 from './img/socios e inversionistas/tbox.png';
+import imagen10 from './img/socios e inversionistas/svnet.png';
+import imagen11 from './img/socios e inversionistas/soluciones sofis.png';
+import imagen12 from './img/socios e inversionistas/simplexo.png';
+import imagen13 from './img/socios e inversionistas/santillana.png';
+import imagen14 from './img/socios e inversionistas/ricoh.png';
+import imagen15 from './img/socios e inversionistas/qudox.png';
+import imagen16 from './img/socios e inversionistas/pensertrust.png';
+import imagen17 from './img/socios e inversionistas/pbs.png';
+import imagen18 from './img/socios e inversionistas/OIP.webp';
+import imagen19 from './img/socios e inversionistas/microsoft.png'
+import imagen20 from './img/socios e inversionistas/localiza.png';
+import imagen21 from './img/socios e inversionistas/legalitica.png'
+
 export default function PresentacionPage() {
 
   const sections = [
@@ -51,16 +73,62 @@ export default function PresentacionPage() {
     { img: slide5, link: "/backend" }
   ];
 
-  const [currentSlide, setCurrentSlide] = useState(0);
+  
+   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
+const slidesPerView = 2;
 
+useEffect(() => {
+  const interval = setInterval(() => {
+    setCurrentSlideIndex((prev) => (prev + slidesPerView) % slides.length);
+  }, 3000);
+
+  return () => clearInterval(interval);
+}, []);
+
+const visibleSlides = slides.slice(
+  currentSlideIndex,
+  currentSlideIndex + slidesPerView
+);
+
+      /* SLIDER */
+  const imagenes = [
+    { img: imagen1, link: "https://oig.usaid.gov/" },
+    { img: imagen2, link: "https://gizcentroamerica.org/" },
+    { img: imagen3, link: "https://www.witsa.org/" },
+    { img: imagen4, link: "https://ales-lac.org/" },
+    { img: imagen5, link: "https://www.bing.com/ck/a?!&&p=7d92c7b64a90e597286ae7d390c554924f89dc6451f5ff65fc63b917f7226a24JmltdHM9MTc3MzM2MDAwMA&ptn=3&ver=2&hsh=4&fclid=20aa1469-9121-6271-1a0c-021e90a763b0&psq=matias+delgado&u=a1aHR0cHM6Ly93d3cudWptZC5lZHUuc3Yv" },
+    { img:imagen6, link: "https://ugb.edu.sv/"},
+    { img:imagen7, link:"https://ufg.edu.sv/"},
+    { img:imagen8, link:"https://www.uees.edu.sv/"},
+    { img:imagen9, link:"https://www.tboxplanet.com/en/home"},
+    { img:imagen10, link:"http://www.svnet.sv/"},
+    { img:imagen11, link:"http://www.sofis-solutions.com/"},
+    { img:imagen12, link:"https://www.simplexo.tech/"},
+    { img:imagen13, link:"https://identity.santillanaconnect.com/login/"},
+    { img:imagen14, link:"https://www.ricoh-americalatina.com/es"},
+    { img:imagen15, link:"https://qudox.io/"},
+    { img:imagen16, link:"http://pensertrust.com/sp/"},
+    {img:imagen17, link:"https://www.grouppbs.com/"},
+    { img:imagen18, link:"http://www.crecer.com.sv/"},
+    { img:imagen19, link:"https://www.microsoft.com/es-sv/"},
+    { img:imagen20, link:"http://localiza.com.sv/"},
+    { img:imagen21, link:"https://legalitika.com/"},
+
+
+  ];
+  const[currentIndex,setCurrentIndex]=useState(0);
+  const itemsPerView=4;
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev + 1) % slides.length);
-    }, 3000);
+    const interval=setInterval(()=>{
+      setCurrentIndex((prev)=>(prev + itemsPerView) % imagenes.length);
 
-    return () => clearInterval(interval);
+    },3000);
+     return () => clearInterval(interval);
   }, []);
-
+  const visibleImagenes=imagenes.slice(currentIndex,
+    currentIndex +  itemsPerView
+  );
+  
   return (
     <div className="bg-mesh min-h-screen">
 
@@ -123,18 +191,18 @@ export default function PresentacionPage() {
       Organizaciones y aliados que apoyan el desarrollo tecnológico
     </p>
 <br></br>
-    {/* Slider o proveedores del area */}
-    <div className="relative h-[300px] bg-white rounded-2xl shadow-lg overflow-hidden flex items-center justify-center">
+   {/* Slider o proveedores del area */}
+    <div className="flex justify-center gap-10 items-center">
 
-      {slides.map((slide, index) => (
+      {visibleSlides.map((slide, index) =>{
+        const isExternal=slide.link.startsWith("http");
+        return isExternal ? (
         <a
           key={index}
           href={slide.link}
           target="_blank"
           rel="noopener noreferrer"
-          className={`absolute transition-opacity duration-100 ${
-            index === currentSlide ? "opacity-1000" : "opacity-0"
-          }`}
+          
         >
           <img
             src={slide.img}
@@ -142,7 +210,13 @@ export default function PresentacionPage() {
             className="max-h-[180px] object-contain hover:scale-105 transition-transform duration-300"
           />
         </a>
-      ))}
+        ):(
+          <Link key={index} to={slide.link}>
+            <img
+            src={slide.img}
+            className='h-[120px] object-contain hover:scale-110 transition'/>
+          </Link>);
+      })}
 
     </div>
 
@@ -164,25 +238,31 @@ export default function PresentacionPage() {
     </p>
 <br></br>
     {/* Slider o proveedores del area */}
-    <div className="relative h-[300px] bg-white rounded-2xl shadow-lg overflow-hidden flex items-center justify-center">
+    <div className="flex justify-center gap-10 items-center">
 
-      {slides.map((slide, index) => (
+      {visibleImagenes.map((imagen, index) =>{
+        const isExternal=imagen.link.startsWith("http");
+        return isExternal ? (
         <a
           key={index}
-          href={slide.link}
+          href={imagen.link}
           target="_blank"
           rel="noopener noreferrer"
-          className={`absolute transition-opacity duration-100 ${
-            index === currentSlide ? "opacity-1000" : "opacity-0"
-          }`}
+          
         >
           <img
-            src={slide.img}
+            src={imagen.img}
             alt="proveedor"
             className="max-h-[180px] object-contain hover:scale-105 transition-transform duration-300"
           />
         </a>
-      ))}
+        ):(
+          <Link key={index} to={imagen.link}>
+            <img
+            src={imagen.img}
+            className='h-[120px] object-contain hover:scale-110 transition'/>
+          </Link>);
+      })}
 
     </div>
 
