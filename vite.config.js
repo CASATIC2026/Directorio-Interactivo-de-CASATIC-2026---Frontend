c0f8bd90fdbfc930a5349c00a383037878f40a73
santiago suria <brandonmegia612@gmail.com>
2026-04-15 13:46:38 -0600

    acualizacion de vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // 👈 CLAVE
+   
     port: 5175,
     proxy: {
       '/api': {
-          target: 'http://localhost:5000',// 👈 TU IP AQUÍ
+          target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
